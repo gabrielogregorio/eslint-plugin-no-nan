701dffc67a2cddb4fbc309d338c76eb9c4cc9292
Gabriel Gregório <gabriel.gregorio@pravaler.com.br>
2023-11-30 09:25:33 -0300

    fix: export const

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const eslintRule = require('./rules/no-nan');
 import * as utils from './utils';
 
+export const { NumberStrict, parseIntStrict, parseFloatStrict } = utils;
+
 export default {
   rules: { 'no-nan': eslintRule },
   ...utils,
